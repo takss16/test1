5c2c7e09f3dc531e7f24d277ae41caefe47f3c76
takss16 <migueltaksmanugue@gmail.com>
2022-11-24 20:57:36 +0800

    firstCommit

---
+++ dataset.php
@@ -0,0 +1,88 @@
+<?php
+    $arrProducts = array(
+        array(
+            'name'          => "ADIDAS 4DFWD X PARLEY SHOES",
+            'description'   => "Men's Running Shoes <br> Shadow Navy / Cloud White / Impact Orange",
+            'price'         => "14000",
+            'photo1'        => "product1.jpg",
+            'photo2'        => "product2.jpg"
+        ),
+        array(
+            'name'          => "ULTRABOOST 22 SHOES",
+            'description'   => "Men's Running Shoes <br> Wonder Steel / Almost Yellow / Legend Ink",
+            'price'         => "10000",
+            'photo1'        => "product2b.jpg",
+            'photo2'        => "product2a.jpg",
+        ),
+        array(
+            'name'          => "ADIZERO ADIOS PRO 3 SHOES",
+            'description'   => "Men's Running Shoes <br> Pulse Lilac / Cloud White / Solar Orange",
+            'price'         => "12000",
+            'photo1'        => "product3a.jpg",
+            'photo2'        => "product3b.jpg",
+        ),
+        array(
+            'name'          => "ADIDAS 4DFWD PULSE 2 RUNNING SHOES",
+            'description'   => "Men's Running Shoes <br> Cloud White / Core Black / Crystal White",
+            'price'         => "8000",
+            'photo1'        => "product4a.jpg",
+            'photo2'        => "product4b.jpg",
+        ),
+        array(
+            'name'          => "Air Jordan XXXVII PF",
+            'description'   => "Men's Basketball Shoes <br> Light Bone/Black/Dark Concord/Fire Red",
+            'price'         => "9995",
+            'photo1'        => "product5a.jfif",
+            'photo2'        => "product5b.jfif",
+        ),
+        array(
+            'name'          => "Zoom Freak 4 Kolossaio",
+            'description'   => "Men's Basketball Shoes <br> Light Bone/Sail/Sweet Beet/Moon Fossil",
+            'price'         => "7095",
+            'photo1'        => "product6a.jfif",
+            'photo2'        => "product6b",
+        ),
+        array(
+            'name'          => "Giannis Immortality 2",
+            'description'   => "Men's Basketball Shoes <br> White/Deep Royal Blue/Hyper Pink/Metallic Silver",
+            'price'         => "4095",
+            'photo1'        => "product7a.jfif",
+            'photo2'        => "product7b.jfif",
+        ), 
+        array(
+            'name'          => "Luka 1 Next Nature PF",
+            'description'   => "Men's Basketball Shoes <br> Football Grey/Dark Marina Blue/Sail/Volt",
+            'price'         => "6195",
+            'photo1'        => "product8a.jfif",
+            'photo2'        => "product8b.jfif",
+        ),
+        array(
+            'name'          => "JORDAN 1 RETRO LOW OG SP TRAVIS SCOTT REVERSE MOCHA MEN'S",
+            'description'   => "Limitted Edition",
+            'price'         => "92999",
+            'photo1'        => "product9b.png",
+            'photo2'        => "product9a.jpg",
+        ),
+        array(
+            'name'          => "JORDAN 4 RETRO INFRARED MEN'S",
+            'description'   => "Limitted Edition",
+            'price'         => "13199",
+            'photo1'        => "product10a.png",
+            'photo2'        => "product10b.png",
+        ),
+        array(
+            'name'          => "ADIDAS YEEZY 2 BOOST 350 LOW SLATE V2 MEN'S",
+            'description'   => "Limitted Edition",
+            'price'         => "22599",
+            'photo1'        => "product11a.png",
+            'photo2'        => "product11b.png",
+        ),
+        array(
+            'name'          => "ADIDAS YEEZY BOOST 700 WAVE RUNNER MEN'S",
+            'description'   => "Limitted Edition",
+            'price'         => "35875",
+            'photo1'        => "product12a.png",
+            'photo2'        => "product12b.png",
+        )
+    );
+?>
